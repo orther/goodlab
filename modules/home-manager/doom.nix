54026e82e5ba8c91a46ee6f0c549cb53ffe29320
Brandon Orther <brandon@orther.dev>
2025-09-23 09:44:20 -0700

    fix(doom): drop useEmacsOverlay option for compatibility; rely on emacsPackage=pkgs.emacs and in-repo doom.d

--- modules/home-manager/doom.nix
+++ modules/home-manager/doom.nix
@@ -6,9 +6,6 @@
     enable = true;
     # Use upstream Emacs from nixpkgs; swap for emacsMacport if preferred.
     emacsPackage = pkgs.emacs;
-    # Avoid nix-community/emacs-overlay to sidestep overrideScope' mismatch
-    # on some nixpkgs revisions.
-    useEmacsOverlay = false;
     # Provide a minimal Doom config stored in-repo
     doomPrivateDir = ./doom.d;
   };
