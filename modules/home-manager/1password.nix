e4d9fd94aac28336a63e4da05ce17dc8299f9b24
Brandon Orther <brandon@orther.dev>
2025-10-04 10:39:19 -0700

    fix(ssh): quote 1Password IdentityAgent path to resolve SSH config syntax error
    The path 'Group Containers' contains spaces and needs to be quoted in SSH config to avoid 'extra arguments at end of line' errors.

--- modules/home-manager/1password.nix
+++ modules/home-manager/1password.nix
@@ -9,10 +9,73 @@
     # Define a default match block and set IdentityAgent there,
     # avoiding raw extraConfig so HM's assertion is satisfied.
     matchBlocks = {
+      # Repository-specific SSH identity for pep-store
+      "github-pep-store" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "~/.ssh/lab_project_20250702";
+        identitiesOnly = true;
+        extraOptions = {
+          AddKeysToAgent = "yes";
+        };
+      };
+
+      # Corporate GitHub and internal hosts
+      "github.com 10.4.0.29 10.4.0.198" = {
+        identitiesOnly = true;
+        identityFile = "/Users/orther/.ssh/id_rsa-arson";
+        extraOptions = {
+          AddKeysToAgent = "yes";
+        };
+      };
+
+      # Corporate bastion host
+      "bastion.hq-staging.aws.carecar.co" = {
+        identitiesOnly = true;
+        identityFile = "/Users/orther/.ssh/id_rsa-arson";
+        user = "borther";
+        extraOptions = {
+          AddKeysToAgent = "yes";
+        };
+      };
+
+      # Research Relay server
+      "104.194.132.119" = {
+        user = "root";
+        identityFile = "~/.ssh/lab_project_20250702";
+        identitiesOnly = true;
+        extraOptions = {
+          AddKeysToAgent = "yes";
+        };
+      };
+
+      # Research Relay shorthand
+      "rr" = {
+        hostname = "104.194.132.119";
+        user = "root";
+        identityFile = "~/.ssh/lab_project_20250702";
+        identitiesOnly = true;
+        extraOptions = {
+          AddKeysToAgent = "yes";
+        };
+      };
+
+      # Research Relay Deploy shorthand
+      "drr" = {
+        hostname = "104.194.132.119";
+        user = "deploy";
+        identityFile = "~/.ssh/lab_project_20250702";
+        identitiesOnly = true;
+        extraOptions = {
+          AddKeysToAgent = "yes";
+        };
+      };
+
+      # Default 1Password agent for all other hosts
       "*" = {
         extraOptions = lib.mkMerge [
           (lib.mkIf pkgs.stdenv.isLinux {IdentityAgent = "~/.1password/agent.sock";})
-          (lib.mkIf pkgs.stdenv.isDarwin {IdentityAgent = "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";})
+          (lib.mkIf pkgs.stdenv.isDarwin {IdentityAgent = "\"~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock\"";})
         ];
       };
     };
